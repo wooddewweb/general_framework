98129728ea7ff04558fff3050251e4051685b9ea
Bruno P. Gonçalves <scorninpc@localhost.localdomain>
2016-12-02 21:43:49 -0200

    Adicionado o diretório data com os diretórios dos templates do smarty Removido os arquivos de teste e exemplos Adicionado o controlador index padrão com a action index padrão

---
+++ app/src/Main/Controller/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Main\Controller;
+
+use Main\Model;
+use General\Db\Adapter;
+
+/**
+ * Controlador padrão
+ */
+class IndexController extends \General\Mvc\Controller
+{
+	/**
+	 * Ação do denied
+	 */
+	public function indexAction()
+	{
+	
+	}
+}
